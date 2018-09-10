b9958b6176fe6c6318f838c0ff137a42438dd9c0
Fahim1988 <fahim.pdc88@gmail.com>
2018-09-10 03:49:36 -0400

    commiting with some test cases

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -2,18 +2,26 @@ package math.problems;
 
 public class Fibonacci {
     public static void main(String[] args) {
+        Fibonacci f1=new Fibonacci();
+        f1.fibo(40);
 
-        int[] level=new int[40];
-        level[0]=0;
-        level[1]=1;
-        for(int i=2;i<level.length;i++){
-           level[i]= level[i-1]+level[i-2];
-            System.out.println(level[i]);
         }
+
+        int [] fibo(int n) {
+        int ret=0;
+            int[] level = new int[n];
+            level[0] = 0;
+            level[1] = 1;
+            for (int i = 2; i < level.length; i++) {
+                level[i] = level[i - 1] + level[i - 2];
+                System.out.println(level[i]);
+
+
+            }
          /*
           Write 40 Fibonacci numbers with java.
          */
 
-
-    }
+          return level;
+        }
 }
--- src/math/problems/UnitTestingMath.java
+++ src/math/problems/UnitTestingMath.java
@@ -1,7 +1,16 @@
+
+
 package math.problems;
+import org.testng.Assert;
 
 public class UnitTestingMath {
     public static void main(String[] args) {
+        Factorial fnum=new Factorial();
+        int result1=Factorial.factorial(5);
+        Assert.assertEquals(120,result1);
+        FindLowestDifference lno=new FindLowestDifference();
+        PrimeNumber pm=new PrimeNumber();
+        Assert.assertEquals(true,PrimeNumber.isPrime(5));
         //Apply Unit testing into each classes and methods in this package.
 
     }
