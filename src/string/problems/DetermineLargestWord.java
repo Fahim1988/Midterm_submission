6042a10ddba173a66ccf10739e2eab96b362a489
Fahim1988 <fahim.pdc88@gmail.com>
2018-09-11 14:38:31 -0400

    string complete

--- src/string/problems/DetermineLargestWord.java
+++ src/string/problems/DetermineLargestWord.java
@@ -5,13 +5,31 @@ import java.util.Iterator;
 import java.util.Map;
 
 public class DetermineLargestWord {
+
+
+    public static String largestWord(String wordGiven){
+        DetermineLargestWord dt =new DetermineLargestWord();
+        Map<Integer, String> wordNLength = dt.findTheLargestWord(wordGiven);
+        int largest = 0;
+        String largeValue = "";
+        for(Map.Entry<Integer, String> checkLength : wordNLength.entrySet()){
+            if(largest< checkLength.getKey()) {
+                largest = checkLength.getKey();
+                largeValue = checkLength.getValue();
+            }
+        }
+        return largeValue;
+    }
+
+
+
     public static void main(String[] args) {
         /*
          Implement to Find the length and longest word in the given sentence below.
          Should return "10 biological".
          */
         String s="Human brain is a biological learning machine";
-
+        System.out.println(largestWord(s));
         Map<Integer, String> wordNLength = findTheLargestWord(s);
         //implement
 
@@ -21,18 +39,13 @@ public class DetermineLargestWord {
     public static Map<Integer, String> findTheLargestWord(String wordGiven){
         Map<Integer, String> map = new HashMap<Integer, String>();
         //implement
-        String[] word=wordGiven.split(" ");
-        String lw=" ";
-        for(int i=0;i<word.length;i++){
-            for(int j=i;j<word.length;j++){
-                if(word[i].length()>word[j].length()){
-                    lw=word[i];
-                }
-            }
-        }
-        System.out.println(lw);
-        System.out.println(lw.length());
+            String st = "";
+            String[] split = wordGiven.split(" ");
 
-        return map;
-    }
+            for(int i=0;i<split.length;i++){
+                map.put(split[i].length(),split[i]);
+            }
+
+            return map;
+        }
 }
